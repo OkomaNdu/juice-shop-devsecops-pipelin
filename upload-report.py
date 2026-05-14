235036fd0764ca2e1415503e89f967aece21c835
ndu <OkomaNdu>
2026-05-13 21:13:58 -0400

    ci: add upload_reports job for gitleaks report

---
+++ upload-report.py
@@ -0,0 +1,26 @@
+import requests
+
+headers = {
+    'Authorization': f'Token {$DEFECTDOJO_API_KEY}'
+}
+
+url = 'https://demo.defectdojo.org/v2/import-scan/'
+
+data = {
+    'active': True
+    'varified': True
+    'scan_type': 'Gitleaks Scan',
+    'minimum_severity': 'low'
+    'engagement': 4
+}
+
+files = {
+    'file': open('Gitleaks.json', 'rb' )
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code ==2021:
+    print('scan results imported successfully')
+else:
+    print(f'failed to import scan results: {response.content}')    
