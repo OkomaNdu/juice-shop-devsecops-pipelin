50af4f11a58bb661dc4d7537d367ea13e51beef2
ndu <OkomaNdu>
2026-05-13 21:35:04 -0400

    updated upload-report

--- upload-report.py
+++ upload-report.py
@@ -1,26 +1,26 @@
 import requests
 
 headers = {
-    'Authorization': f'Token {$DEFECTDOJO_API_KEY}'
+    'Authorization': f'Token {os.environ["DEFECTDOJO_API_KEY"]}'
 }
 
 url = 'https://demo.defectdojo.org/v2/import-scan/'
 
 data = {
-    'active': True
-    'varified': True
+    'active': True,
+    'varified': True,
     'scan_type': 'Gitleaks Scan',
     'minimum_severity': 'low'
     'engagement': 4
 }
 
 files = {
-    'file': open('Gitleaks.json', 'rb' )
+    'file': open('gitleaks.json', 'rb')
 }
 
 response = requests.post(url, headers=headers, data=data, files=files)
 
-if response.status_code ==2021:
+if response.status_code ==201:
     print('scan results imported successfully')
 else:
     print(f'failed to import scan results: {response.content}')    
